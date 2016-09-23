088b0d90d9a56b535695ee717ae358382d07fee3
cholid ridwan <cholid@id.edge-asia.com>
2016-09-23 18:35:43 +0700

    add text view to set id button

--- app/src/main/java/com/edgeasia/tes03/MainActivity.java
+++ app/src/main/java/com/edgeasia/tes03/MainActivity.java
@@ -1,26 +1,31 @@
 package com.edgeasia.tes03;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private int i = 0;
-    //private CustButton btnTag;
     private int btnId = 0;
-
-
     private RelativeLayout canvas;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText)findViewById(R.id.inputNumber);
 
         canvas = (RelativeLayout)findViewById(R.id.canvas);
         canvas.setOnClickListener(new View.OnClickListener() {
@@ -42,23 +47,28 @@ public class MainActivity extends AppCompatActivity {
                 } else if (i == 2) {
                     //Double click
                     i = 0;
-                    btnId++;
-                    Button btnTag = new Button(getApplicationContext());
-                    btnTag.setText("Button" + String.valueOf(btnId));
-                    btnTag.setY(btnId*100);
-                    btnTag.setId(btnId);
-                    btnTag.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Log.i("button number", String.valueOf(v.getId()));
-                        }
-                    });
-                    canvas.addView(btnTag, new RelativeLayout.LayoutParams(
-                            RelativeLayout.LayoutParams.WRAP_CONTENT,
-                            RelativeLayout.LayoutParams.WRAP_CONTENT)
-                    );
+                    createButton(Integer.parseInt(editText.getText().toString()));
                 }
             }
         });
+    }
+
+    private void createButton(int i){
+        btnId++;
+        Button btnTag = new Button(getApplicationContext());
+        btnTag.setText("Button" + String.valueOf(btnId));
+        btnTag.setY(btnId*150);
+        btnTag.setId(i);
+        btnTag.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.i("button number", String.valueOf(v.getId()));
+            }
+        });
+        canvas.addView(btnTag, new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT)
+        );
     }
 }
